d7099c4260a96b059d05c6292ae29483cd8a264b
rjpc <ryan@rjpc.net>
2023-12-05 13:46:09 -0800

    first commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Flake for building a Raspberry Pi Zero 2 SD images";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    deploy-rs.url = "github:serokell/deploy-rs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    deploy-rs,
+  }: rec {
+    nixosConfigurations = {
+      zero2w = nixpkgs.lib.nixosSystem {
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          ./zero2w.nix
+        ];
+      };
+      zeroCool2w = nixpkgs.lib.nixosSystem {
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          ./zeroCool2w.nix
+        ];
+      };
+    };
+
+    deploy = {
+      user = "root";
+      nodes = {
+        zero2w = {
+          hostname = "nemo";
+          profiles.system.path =
+            deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.zero2w;
+        };
+        zeroCool2w = {
+          hostname = "zerocool";
+          profiles.system.path =
+            deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.zeroCool2w;
+        };
+      };
+    };
+  };
+}
