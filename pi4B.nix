fcc4c0ffcaebb34239a2bc342acaacf07a53545d
rjpc <ryan@rjpc.net>
2024-01-20 13:43:18 -0800

    cleanup but I gotta make sure this stuff still works later today

--- pi4B.nix
+++ pi4B.nix
@@ -1,14 +1,15 @@
-{ pkgs, ... }: {  
-networking = {
+{ pkgs, ... }: {
+
+  networking = {
     interfaces."wlan0".useDHCP = true;
     wireless = {
       enable = true;
       userControlled.enable = true;
       interfaces = [ "wlan0" ];
       networks = {
-        "XXXXX" = {
+        "XXXXXX" = {
           hidden = true;
-          psk = "XXXXX";
+          psk = "XXXXXX";
         };
       };
     };
@@ -16,52 +17,75 @@ networking = {
 
   environment.systemPackages = with pkgs; [
     neofetch
-    osv-scanner
-    go
-    vim
   ];
+  environment.pathsToLink = [ "/share/zsh" ];
 
-  services.sshd.enable = true;
-  # TODO: move to common
-  # NTP time sync.
-  services.timesyncd.enable = true;
-  services.xserver = {
+  programs.zsh.interactiveShellInit = ''
+    source ${pkgs.zsh-git-prompt}/share/zsh-git-prompt/zshrc.sh
+  '';
+  programs.git = {
     enable = true;
-    layout = "us";
-    autorun = true;
-    exportConfiguration = true;
-    displayManager.startx.enable = false;
-    displayManager.autoLogin.enable = false;
-    displayManager.autoLogin.user = "rjpc";
-    displayManager.lightdm = {
-      enable = true;
-      background = pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath;
-      greeters.gtk.indicators = [
-        "~host"
-        "~spacer"
-        "~clock"
-        "~spacer"
-        "~session"
-        "~power"
-      ];
-      greeters.gtk.clock-format = "%A %F %I:%M %p";
-    };
-    windowManager.cwm.enable = true;
+    package = pkgs.git;
+    prompt.enable = true;
   };
-
-  users.users.rjpc = {
-    isNormalUser = true;
-    home = "/home/rjpc";
-    description = "Ryan J.P. Casalino";
-    password = "guess";
-    extraGroups = [ "wheel" "networkmanager" ];
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNo24kFmOht87GEejqv4uWquucROWu4Fw8v8JaElomJ rjpc@zits" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDRD43YeRHIv/H4S8Hj9bw0uoGRo0W9mCMMOZvtHPBLi rjpc@air" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMM2gabh4hExixOKrLfrG029dA5TiKyr4SZB5BsJB65o rjpc@YF21" ];
-  };
-
-  security.sudo = {
-    enable = true;
-    wheelNeedsPassword = false;
-  };
-
-  services.getty.autologinUser = "rjpc";
+  # TODO:
+  # need to hold off until I move over the eth and get IPs
+  # set and locked and then I can replace the aging Pi3
+  #  security.acme.acceptTerms = true;
+  #  security.acme.defaults.email = "ryan@rjpc.net";
+  #  services.nginx = {
+  #    enable = true;
+  #    recommendedProxySettings = true;
+  #    recommendedTlsSettings = true;
+  #
+  #    commonHttpConfig = ''
+  #      # Add HSTS header with preloading to HTTPS requests.
+  #      # Adding this header to HTTP requests is discouraged
+  #      map $scheme $hsts_header {
+  #          https   "max-age=31536000; includeSubdomains; preload";
+  #      }
+  #      add_header Strict-Transport-Security $hsts_header;
+  #
+  #      # Minimize information leaked to other domains
+  #      add_header 'Referrer-Policy' 'origin-when-cross-origin';
+  #
+  #      # (Disable/Enable) embedding as a frame
+  #      add_header X-Frame-Options SAMEORIGIN always;
+  #
+  #      # Prevent injection of code in other mime types (XSS Attacks)
+  #      add_header X-Content-Type-Options nosniff;
+  #
+  #      # Enable XSS protection of the browser.
+  #      # May be unnecessary when CSP is configured properly (see above)
+  #      add_header X-XSS-Protection "1; mode=block";
+  #    '';
+  #
+  #    virtualHosts."blog.rjpc.net" = {
+  #      enableACME = true;
+  #      forceSSL = true;
+  #      root = "/var/www/blog_rjpc_net";
+  #      locations."/archive/2019/anniversary_2019.html".extraConfig = ''
+  #        auth_basic "Anna's Area";
+  #        auth_basic_user_file /etc/apache2/.htpasswd;
+  #      '';
+  #      locations."/archive".extraConfig = ''
+  #        autoindex on;
+  #        autoindex_exact_size off;
+  #      '';
+  #      locations."/cv".extraConfig = ''
+  #        autoindex on;
+  #        autoindex_exact_size off;
+  #      '';
+  #    };
+  #    virtualHosts."www.rjpc.net" = {
+  #      enableACME = true;
+  #      forceSSL = true;
+  #      root = "/var/www/web_rjpc_net";
+  #    };
+  #    virtualHosts."rjpc.net" = {
+  #      enableACME = true;
+  #      forceSSL = true;
+  #      root = "/var/www/web_rjpc_net";
+  #    };
+  #  };
 }
