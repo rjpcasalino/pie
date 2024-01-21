4823de1c059496055e3e44cb559f3a5f21c7fa5b
rjpc <ryan@rjpc.net>
2024-01-21 14:02:58 -0800

    update how to set options and add hostname and shell but FIXME too

--- zero2w.nix
+++ zero2w.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
 
+  users.users.rjpc.shell = if config.shell == "bash" then "${pkgs.bashInteractive}${pkgs.bashInteractive.shellPath}" else "${pkgs.zsh}${pkgs.zsh.shellPath}";
+  networking.hostName = config.hostname;
   environment.systemPackages = with pkgs; [
     neofetch
     xscreensaver
